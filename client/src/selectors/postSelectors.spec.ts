7f10dbabb9b9ff0d2b588ee0c46181f5e8f1a0ea
Mikhail Pontus <m.pontus@gmail.com>
2018-09-06 20:03:20 +0300

    Update post selectors

--- client/src/selectors/postSelectors.spec.ts
+++ client/src/selectors/postSelectors.spec.ts
@@ -1,50 +1,95 @@
+import reducer, { State } from "../reducers";
 import { makeGetPostIds, makeGetPostById } from "./postSelectors";
 
-describe("makeGetPostIds", () => {
-  const getPostIds = makeGetPostIds();
+const postFixture = {
+  id: "5",
+  picture: {
+    url:
+      "https://images.unsplash.com/photo-1535412833400-85426926b8c1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=47e717c53dae51ed4a300fffa13733a8&auto=format&fit=crop&w=500&q=60",
+    width: 500,
+    height: 333
+  },
+  author: {
+    id: "auth9|123123",
+    name: "Foo bar",
+    avatarUrl:
+      "https://images.unsplash.com/profile-1532310311737-e56bb5caa506?dpr=1&auto=format&fit=crop&w=64&h=64&q=60&crop=faces&bg=fff"
+  },
+  likesCount: 1,
+  isLiked: true,
+  timestamp: 1535731213512
+};
+const postFixture2 = {
+  id: "7",
+  picture: {
+    url:
+      "https://images.unsplash.com/photo-1535412833400-85426926b8c1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=47e717c53dae51ed4a300fffa13733a8&auto=format&fit=crop&w=500&q=60",
+    width: 500,
+    height: 333
+  },
+  author: {
+    id: "auth9|123123",
+    name: "Foo bar",
+    avatarUrl:
+      "https://images.unsplash.com/profile-1532310311737-e56bb5caa506?dpr=1&auto=format&fit=crop&w=64&h=64&q=60&crop=faces&bg=fff"
+  },
+  likesCount: 1,
+  isLiked: true,
+  timestamp: 1535731213512
+};
+const initialState = reducer(undefined, {} as any);
 
-  it("Returns post ids", () => {
-    const state = {
-      posts: {
-        ids: ["3", "5", "9"]
-      }
-    };
+describe("postSelectors", () => {
+  describe("makeGetPostIds", () => {
+    const getPostIds = makeGetPostIds();
 
-    expect(getPostIds(state as any)).toEqual(["3", "5", "9"]);
-  });
-});
-
-describe("makeGetPostById", () => {
-  const getPostById = makeGetPostById();
-
-  it("Returns post ids", () => {
-    const post = {
-      id: "5",
-      picture: {
-        url:
-          "https://images.unsplash.com/photo-1535412833400-85426926b8c1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=47e717c53dae51ed4a300fffa13733a8&auto=format&fit=crop&w=500&q=60",
-        width: 500,
-        height: 333
-      },
-      author: {
-        id: "auth9|123123",
-        name: "Foo bar",
-        avatarUrl:
-          "https://images.unsplash.com/profile-1532310311737-e56bb5caa506?dpr=1&auto=format&fit=crop&w=64&h=64&q=60&crop=faces&bg=fff"
-      },
-      likesCount: 1,
-      isLiked: true,
-      timestamp: 1535731213512
-    };
-
-    const state = {
-      posts: {
-        byId: {
-          "5": post
+    it("Returns all post ids", () => {
+      const state = {
+        ...initialState,
+        posts: {
+          ...initialState.posts,
+          pendingIds: ["2", "4"],
+          ids: ["3", "5", "9"]
         }
-      }
-    };
+      };
 
-    expect(getPostById(state as any, { id: "5" })).toEqual(post);
+      expect(getPostIds(state)).toEqual(["2", "4", "3", "5", "9"]);
+    });
+  });
+
+  describe("makeGetPostById", () => {
+    const getPostById = makeGetPostById();
+
+    it("should return post by its id", () => {
+      const state = {
+        ...initialState,
+        posts: {
+          ...initialState.posts,
+          byId: {
+            [postFixture.id]: postFixture
+          }
+        }
+      };
+
+      expect(getPostById(state, { id: postFixture.id })).toEqual(postFixture);
+    });
+
+    it("should resolve pending post instance if it exists", () => {
+      const state: State = {
+        ...initialState,
+        posts: {
+          ...initialState.posts,
+          instances: {
+            [postFixture.id]: postFixture2.id
+          },
+          byId: {
+            [postFixture.id]: postFixture,
+            [postFixture2.id]: postFixture2
+          }
+        }
+      };
+
+      expect(getPostById(state, { id: "5" })).toEqual(postFixture2);
+    });
   });
 });
--- client/src/selectors/postSelectors.ts
+++ client/src/selectors/postSelectors.ts
@@ -1,21 +1,47 @@
-import { Selector, createSelector } from "reselect";
+import { Selector, ParametricSelector, createSelector } from "reselect";
 import { State } from "../reducers";
 import { Post } from "../models";
 
+/**
+ * Resolve Post's final id
+ *
+ * Posts are created instantly after submission with auto-generated
+ * id. After saving the post to the database, that id is aliased with
+ * the real id by which the entity returned from the API is saved into
+ * redux store.
+ */
+const makeGetFinalId = (): ParametricSelector<State, string, string> =>
+  createSelector(
+    state => state.posts.instances,
+    (state: State, id: string) => id,
+    (instances, id) => {
+      const recurse = (id: string): string =>
+        id in instances ? recurse(instances[id]) : id;
+
+      return recurse(id);
+    }
+  );
+
+/**
+ * Return the list IDs in the current listing
+ *
+ * Do not return final ids from this function to avoid remounting
+ * pending posts after resolution.
+ */
 export const makeGetPostIds = (): Selector<
   State,
   ReadonlyArray<string>
-> => state => state.posts.ids;
+  > => state => [...state.posts.pendingIds, ...state.posts.ids];
 
-export const makeGetPostById = () =>
-  createSelector<
-    State,
-    { id: string },
-    string,
-    { [id: string]: Post | undefined },
-    Post | undefined
-  >(
-    (_, ownProps) => ownProps.id,
-    state => state.posts.byId,
-    (id, byId) => byId[id]
-  );
+/**
+ * Retrieve the post by ID and denormalize it into domain model
+ */
+export const makeGetPostById = (): ParametricSelector<
+  State,
+  { id: string },
+  Post | undefined
+  > => {
+  const getFinalId = makeGetFinalId();
+
+  return (state, { id }) => state.posts.byId[getFinalId(state, id)];
+};
