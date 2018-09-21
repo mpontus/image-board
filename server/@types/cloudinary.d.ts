37e6596687f786fd5604aa3b32d53bc2aa2fb9ed
Mikhail Pontus <m.pontus@gmail.com>
2018-09-21 11:44:45 +0300

    Update cloudinary typings

--- server/@types/cloudinary.d.ts
+++ server/@types/cloudinary.d.ts
@@ -1,8 +1,65 @@
 declare module "cloudinary" {
-  const cloudinary: cloudinary.Cloudinary;
+  type PublicId = string;
+
+  type Callback<T> = ((err: Error) => void) | ((err: void, result: T) => void);
 
   namespace cloudinary {
-    export type Cloudinary = any;
+    interface Config {
+      cloud_name?: string;
+      api_key?: string;
+      api_secret?: string;
+      private_cdn?: string;
+      secure_distribution?: string;
+    }
+
+    function config(config: Config): Config;
+    function config<K extends keyof Config>(key: K, value: Config[K]): Config;
+
+    namespace v2 {
+      namespace uploader {
+        // This list is incomplete, see https://cloudinary.com/documentation/image_upload_api_reference#upload
+        interface UploadOptions {
+          folder: string;
+        }
+
+        // This list is incomplete
+        interface UploadResult {
+          public_id: PublicId;
+          url: string;
+          width: number;
+          height: number;
+        }
+
+        function upload(
+          file: Buffer,
+          callback?: Callback<UploadResult>,
+          options?: UploadOptions
+        ): Promise<UploadResult>;
+
+        function upload_stream(
+          callback: Callback<UploadResult>
+        ): NodeJS.WriteStream;
+
+        function upload_stream(
+          options: UploadOptions,
+          callback: Callback<UploadResult>
+        ): NodeJS.WriteStream;
+
+        // This list is incomplete
+        interface DestroyOptions {
+          invalidate: boolean;
+        }
+
+        // This list is empty because we don't care about the response
+        interface DestroyResult {}
+
+        function destroy(
+          publicId: PublicId,
+          options: DestroyOptions,
+          callback?: Callback<DestroyResult>
+        ): Promise<DestroyResult>;
+      }
+    }
   }
 
   export = cloudinary;
